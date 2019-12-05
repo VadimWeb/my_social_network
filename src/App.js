6895d94e34fef5c4de4746cb1878d9577d9f3c32
VadimWeb <vad.cudin2011@yandex.ru>
2019-12-05 18:59:35 +0300

    add CSS-styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 
 const App = () => {
     return (
-        <div className="App">
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://dcassetcdn.com/design_img/1559024/551167/551167_7840631_1559024_911ff84c_image.png' />
+            </header>
+            <nav className='nav'>
+                <div>Profile</div>
+                <div>Messages</div>
+            </nav>
+            <div className='content'>
+                Main Content
+            </div>
 
         </div>
     );
