039090e892771625b2ef07770c6187ed6458561e
VadimWeb <vad.cudin2011@yandex.ru>
2019-12-05 19:44:28 +0300

    crushing into components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Profile from "./Components/Profile";
 
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src='https://dcassetcdn.com/design_img/1559024/551167/551167_7840631_1559024_911ff84c_image.png' />
-            </header>
-            <nav className='nav'>
-                <div> <a href=""> Profile</a></div>
-                <div><a href=""> Messages</a></div>
-                <div><a href=""> News</a></div>
-                <div><a href=""> Music</a></div>
-                <div><a href=""> Settings</a></div>
-            </nav>
-            <div className='content'>
-                <img src="https://klike.net/uploads/posts/2019-01/1547365376_1.jpg" alt=""/>
-            </div>
-
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/Components/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img src="https://klike.net/uploads/posts/2019-01/1547365376_1.jpg" alt=""/>
+            </div>
+            <div><img
+                src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                alt=""/></div>
+            <div> ava + descriotion</div>
+            <div> My Posts
+                <div> New Posts </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
