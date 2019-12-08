a129a343db8640865bba34dcea34d22af61eedbd
VadimWeb <vad.cudin2011@yandex.ru>
2019-12-08 21:00:27 +0300

    added Dialogs

--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -3,9 +3,21 @@ import style from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-     <div className={style.contetn}>
-         Dialogs
-     </div>
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>Алинка</div>
+                <div className={style.dialog + ' ' + style.active}>Егор</div>
+                <div className={style.dialog + ' ' + style.active}>Евгений</div>
+                <div className={style.dialog + ' ' + style.active}>Елена</div>
+                <div className={style.dialog + ' ' + style.active}>Ренат</div>
+                <div className={style.dialog + ' ' + style.active}>Варвара</div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How is your it-kamasutra?</div>
+                <div className={style.message}>Yo</div>
+            </div>
+        </div>
     );
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import style from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={style.nav}>
-            <div className={style.item}><a href="/profile"> Profile </a></div>
-            <div className={style.item}><a href="/dialogs"> Messages </a></div>
-            <div className={style.item}><a href="/news"> News </a></div>
-            <div className={style.item}><a href="/music"> Music </a></div>
-            <div className={style.item}><a href="/settings"> Settings </a></div>
+            <div className={style.item}><NavLink to="/profile" activeClassName={style.active}> Profile </NavLink></div>
+            <div className={style.item}><NavLink to="/dialogs" activeClassName={style.active}> Messages </NavLink></div>
+            <div className={style.item}><NavLink to="/news" activeClassName={style.active}> News </NavLink></div>
+            <div className={style.item}><NavLink to="/music" activeClassName={style.active}> Music </NavLink></div>
+            <div className={style.item}><NavLink to="/settings" activeClassName={style.active}> Settings </NavLink></div>
         </nav>
     );
 }
