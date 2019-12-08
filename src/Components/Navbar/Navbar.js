7074eba0cbc393b166cf5e23079e338f10a7fd31
VadimWeb <vad.cudin2011@yandex.ru>
2019-12-08 20:13:50 +0300

    added Route

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
         <nav className={style.nav}>
             <div className={style.item}><a href="/profile"> Profile </a></div>
             <div className={style.item}><a href="/dialogs"> Messages </a></div>
-            <div className={style.item}><a href="#"> News </a></div>
-            <div className={style.item}><a href="#"> Music </a></div>
-            <div className={style.item}><a href="#"> Settings </a></div>
+            <div className={style.item}><a href="/news"> News </a></div>
+            <div className={style.item}><a href="/music"> Music </a></div>
+            <div className={style.item}><a href="/settings"> Settings </a></div>
         </nav>
     );
 }
---
+++ src/Components/News/News.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './News.module.css'
+
+const News = () => {
+    return (
+     <div className={style.contetn}>
+         News
+     </div>
+    );
+}
+
+export default News;
